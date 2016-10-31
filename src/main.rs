b2bbf1fadf1bae159252a7971b8a2915baf197c7
Will Salisbury <wjs@uber.com>
2016-10-31 12:31:04 -0700

    push!

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
+use std::mem;
+
+type Data = i32;
+
 #[derive(Debug)]
-enum LinkedList {
-    Node {data: i32, next: Box<LinkedList>},
+enum Node {
+    Head(Data, Box<Node>),
     Tail
 }
 
-impl LinkedList {
-    fn new(data: i32, next: LinkedList) -> LinkedList {
-        LinkedList::Node{data: data, next: Box::new(next)}
-    }
-
-    fn tail() -> LinkedList {
-        LinkedList::Tail
+impl Node {
+    fn new(data: Data) -> Self {
+        Node::Head(data, Box::new(Node::Tail))
     }
 }
 
+
+#[derive(Debug)]
+struct List {
+    head: Node
+}
+
+impl List {
+
+    fn new(data: Data) -> Self {
+        List{head: Node::new(data)}
+    }
+
+    fn push(&mut self, data: Data) {
+        self.head = Node::Head(data, Box::new(mem::replace(&mut self.head, Node::Tail)));
+    }
+
+    fn peek(&self) -> Option<Data> {
+        match self.head {
+            Node::Head(data, ref tail) => Some(data),
+            Node::Tail => None
+        }
+    }
+
+}
+
 fn main() {
-    let list: LinkedList = LinkedList::new(1, LinkedList::new(2, LinkedList::tail()));
+    let mut list = List::new(1);
+    println!("{:?}", list);
+    println!("{:?}", list.peek());
+    list.push(2);
     println!("{:?}", list);
 }
